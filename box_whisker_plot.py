72ec076868fe52b7306d6b78233e714db5b0f059
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-05-17 23:58:14 +0500

    Box plot props

--- box_whisker_plot.py
+++ box_whisker_plot.py
@@ -4,6 +4,6 @@ plt.boxplot(x,notch=True)
 plt.show()
 plt.boxplot(x,vert=True,widths=0.2,label=['python'],patch_artist=True,showmeans=True)
 plt.show()
-x1=[10,20,30,40,50,60,70,120]
-plt.boxplot(x1)
+x1=[10,20,30,40,50,60,70,120]   #outlier show ho ga
+plt.boxplot(x1,sym="green",boxprops=dict(color="r"),capprops=dict(color="yellow"))    #whis=2 se join ho jae ga outlier
 plt.show()
