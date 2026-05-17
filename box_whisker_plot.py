3664a3064de87e0c21727550a07d73f98de8af5c
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-05-18 00:06:13 +0500

    multiple box plots

--- box_whisker_plot.py
+++ box_whisker_plot.py
@@ -5,5 +5,15 @@ plt.show()
 plt.boxplot(x,vert=True,widths=0.2,label=['python'],patch_artist=True,showmeans=True)
 plt.show()
 x1=[10,20,30,40,50,60,70,120]   #outlier show ho ga
-plt.boxplot(x1,sym="green",boxprops=dict(color="r"),capprops=dict(color="yellow"))    #whis=2 se join ho jae ga outlier
+plt.boxplot(x1,sym="green",boxprops=dict(color="r"),capprops=dict(color="yellow"),whiskerprops=dict(color="red"),flierprops=dict(markeredgecolor="yellow"))    #whis=2 se join ho jae ga outlier
+plt.show()
+
+
+# multiple box plot
+import matplotlib.pyplot as plt
+x1=[10,20,30,40,50,60,70]
+x2=[10,30,20,40,60,90,50]
+y=[x1,x2]
+plt.boxplot(y,labels=["python","c++"],showmeans=True,sym="green",boxprops=dict(color="green"),capprops=dict(color="yellow"))
+plt.legend(loc="best")
 plt.show()
